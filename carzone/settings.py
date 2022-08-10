31b6f2ac837878fdbd643f1ca2338c0c40fa29b3
Joel <aholoujoel5@gmail.com>
2022-08-10 18:37:17 +0000

    Edited settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -162,6 +162,6 @@ LOGIN_REDIRECT_URL = '/accounts/dashboard'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'aholoujoel5@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ezekiel02'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
